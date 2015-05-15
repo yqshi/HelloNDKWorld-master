c7306930d61bb37b2c0b3e9859c3226505134a0a
yqshi <939432817@qq.com>
2015-05-15 18:47:38 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+HelloNDKWorld
+=============
+
+Hello world using NDK in gradle and android studio
+
+Fill in your ndk folde in local properties with the following line
+ndk.dir=your_ndk_location
+
+This app has been updated to build the ndk part as suggested in the gradle examples. Before NDK was supported by the android gradle plugin, this project could build the ndk files form targets defined in the gradle file.
---
+++ lib/src/main/jni/native.c
@@ -0,0 +1,36 @@
+#include <jni.h>
+#include <string.h>
+#include <android/log.h>
+#define DEBUG_TAG "NDK_AndroidNDK1SampleActivity"
+void Java_com_example_hellondkworld_MainActivity_helloLog(JNIEnv * env, jobject this, jstring logThis)
+{
+    jboolean isCopy;
+    const char * szLogThis = (*env)->GetStringUTFChars(env, logThis, &isCopy);
+    __android_log_print(ANDROID_LOG_DEBUG, DEBUG_TAG, "NDK:LC: [%s]", szLogThis);
+    (*env)->ReleaseStringUTFChars(env, logThis, szLogThis);
+}
+
+jstring
+Java_com_example_hellondkworld_MainActivity_stringFromJNI( JNIEnv* env,
+                                                  jobject thiz )
+{
+#if defined(__arm__)
+  #if defined(__ARM_ARCH_7A__)
+    #if defined(__ARM_NEON__)
+      #define ABI "armeabi-v7a/NEON"
+    #else
+      #define ABI "armeabi-v7a"
+    #endif
+  #else
+   #define ABI "armeabi"
+  #endif
+#elif defined(__i386__)
+   #define ABI "x86"
+#elif defined(__mips__)
+   #define ABI "mips"
+#else
+   #define ABI "unknown"
+#endif
+
+    return (*env)->NewStringUTF(env, "Hello from JNI !  Compiled with ABI " ABI ".");
+}
